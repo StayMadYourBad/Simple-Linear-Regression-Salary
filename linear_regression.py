3e6594436e198f558c9aa1f38b5f232d3d7f8803
Safeer Abbas <safeerabbas3@gmail.com>
2021-06-14 17:42:58 -0400

    Added plot for train and test data

--- linear_regression.py
+++ linear_regression.py
@@ -23,9 +23,32 @@ y = dataset.iloc[:, -1].values #Taking the last column
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, test_size=0.2)
 
 # ---------------------------------------------------------------------------- #
-#                               Training the Data                              #
+#                         Training and Predicting Data                         #
 # ---------------------------------------------------------------------------- #
 
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 y_pred = regressor.predict(X_test) #y_pred is the y predicted values
+
+# ---------------------------------------------------------------------------- #
+#                       Visualizing the Training Data Set                      #
+# ---------------------------------------------------------------------------- #
+
+plt.scatter(X_train, y_train, color='red')
+plt.plot(X_train, regressor.predict(X_train), color = "blue")
+plt.title("Salary vs Experience (Training Set)")
+plt.xlabel("Years of Experience")
+plt.ylabel("Salary")
+plt.show()
+
+
+# ---------------------------------------------------------------------------- #
+#                         Visualizing the Test Data Set                        #
+# ---------------------------------------------------------------------------- #
+
+plt.scatter(X_test, y_test, color='red')
+plt.plot(X_train, regressor.predict(X_train), color = "blue")
+plt.title("Salary vs Experience (Test Set)")
+plt.xlabel("Years of Experience")
+plt.ylabel("Salary")
+plt.show()
